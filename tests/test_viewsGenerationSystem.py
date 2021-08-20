fa4c71d7118278c285c4650b7c1a5ec3c546420a
buddsean <sean@nvaccess.org>
2021-08-20 12:34:08 +1000

    add test data

---
+++ tests/test_viewsGenerationSystem.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2021 NV Access Limited
+# This file may be used under the terms of the GNU General Public License, version 2 or later.
+# For more details see: https://www.gnu.org/licenses/gpl-2.0.html
+
+"""
+Runs the dataView generation system on test data.
+Confirms the end result data is as expected.
+Runs a number of specific checks for scenarios within the test data.
+Also confirms that each output json matches the required schema and path.
+"""
