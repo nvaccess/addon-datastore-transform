70b11cda128947c482e09fe453cf1d2be1dcac50
buddsean <sean@nvaccess.org>
2021-09-03 15:27:33 +1000

    removed unused content

--- src/validate/validate.py
+++ src/validate/validate.py
@@ -5,15 +5,7 @@
 import json
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
-import os
-import tempfile
 import typing
-
-
-JSON_INPUT_SCHEMA = os.path.join(os.path.dirname(__file__), "addonInput_schema.json")
-JSON_VIEW_SCHEMA = os.path.join(os.path.dirname(__file__), "addonView_schema.json")
-JSON_VERSION_SCHEMA = os.path.join(os.path.dirname(__file__), "NVDAVersions_schema.json")
-TEMP_DIR = tempfile.gettempdir()
 
 JsonObjT = typing.Dict[str, typing.Any]
 
