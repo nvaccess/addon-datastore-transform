364d2a1b069c528aa052820aaddb8189a61f31df
buddsean <sean@nvaccess.org>
2021-10-11 11:10:52 +1100

    simplify system tests

--- src/tests/test_viewsGenerationSystem.py
+++ src/tests/test_viewsGenerationSystem.py
@@ -5,11 +5,11 @@
 """
 Runs the dataView generation system on test data.
 Confirms the end result data is as expected.
-Creates a number of specific scenarios for the test data.
+Creates a number of specific scenarios for running the transformation.
 """
 
 from copy import deepcopy
-from dataclasses import dataclass
+from enum import Enum
 import json
 import glob
 from logging import getLogger
@@ -18,49 +18,32 @@ from pathlib import Path
 import shutil
 from src.tests.generateData import MockAddon
 from src.transform.datastructures import Addon, MajorMinorPatch, VersionCompatibility
-from typing import Iterable, Tuple
+from typing import Iterable
 import subprocess
 import unittest
 
-TEST_OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "test_data/output")
+log = getLogger()
 VIEWS_GLOB = "/**/**/*.json"
 
-log = getLogger()
+
+class DATA_DIR(str, Enum):
+	_root = os.path.join(os.path.dirname(__file__), "test_data")
+	INPUT = os.path.join(_root, "input")
+	OUTPUT = os.path.join(_root, "output")
+	EXPECTED_RES = os.path.join(_root, "expected_results")
+	nvdaAPIVersionsPath = os.path.join(INPUT, "nvdaAPIVersions.json")
 
 
-@dataclass
-class TestSet:
-	id: int
-	inputDir: str
-	outputDir: str
+class _TestDataGenerator:
+	_test_nvdaAPIVersions = [
+		VersionCompatibility(MajorMinorPatch(2020, 1), MajorMinorPatch(2020, 1)),
+		VersionCompatibility(MajorMinorPatch(2020, 2), MajorMinorPatch(2020, 1)),
+		VersionCompatibility(MajorMinorPatch(2021, 1), MajorMinorPatch(2021, 1)),
+	]
 
-
-class _GenerateSystemTestData:
-	"""
-	A class which generates system test data for a given testing set given.
-	"""
-	testSet: TestSet
-
-	def __init__(self, testSet: TestSet) -> None:
-		self.testSet = testSet
-		self._test_nvdaAPIVersions = [
-			VersionCompatibility(MajorMinorPatch(2020, 1), MajorMinorPatch(2020, 1)),
-			VersionCompatibility(MajorMinorPatch(2020, 2), MajorMinorPatch(2020, 1)),
-			VersionCompatibility(MajorMinorPatch(2020, 3), MajorMinorPatch(2020, 1)),
-			VersionCompatibility(MajorMinorPatch(2020, 4), MajorMinorPatch(2020, 4)),
-			VersionCompatibility(MajorMinorPatch(2021, 1), MajorMinorPatch(2021, 1)),
-		]
-		if self.testSet.id == 1:
-			self._test_nvdaAPIVersions.append(
-				VersionCompatibility(MajorMinorPatch(2021, 2), MajorMinorPatch(2021, 1))
-			)
-		if Path(self.testSet.inputDir).exists():
-			shutil.rmtree(self.testSet.inputDir)
-		if Path(self.testSet.outputDir).exists():
-			shutil.rmtree(self.testSet.outputDir)
-		self.write_nvdaAPIVersions()
-
-	def write_mock_addon_to_files(self, addon: Addon, exceptedVersions: Iterable[MajorMinorPatch]):
+	@staticmethod
+	def write_mock_addon_to_files(addon: Addon, exceptedVersions: Iterable[MajorMinorPatch]):
+		"""Write mock addon data to the input and expected results directories."""
 		addonData = {
 			"addonId": addon.addonId,
 			"addonVersionNumber": addon.addonVersion._asdict(),
@@ -68,180 +51,124 @@ class _GenerateSystemTestData:
 			"lastTestedVersion": addon.lastTestedVersion._asdict(),
 			"channel": addon.channel,
 		}
-		if addonData["minNVDAVersion"]["patch"] is False:
-			addonData["minNVDAVersion"]["patch"] = 0
-		if addonData["lastTestedVersion"]["patch"] is False:
-			addonData["lastTestedVersion"]["patch"] = 0
-		addonWritePath = f"{self.testSet.inputDir}/{addon.addonId}"
+		addonData["minNVDAVersion"]["patch"] = 0
+		addonData["lastTestedVersion"]["patch"] = 0
+		addonWritePath = os.path.join(DATA_DIR.INPUT, addon.addonId)
 		Path(addonWritePath).mkdir(parents=True, exist_ok=True)
 		with open(f"{addonWritePath}/{str(addon.addonVersion)}.json", "w") as addonFile:
 			json.dump(addonData, addonFile, indent=4)
 		for nvdaAPIVersion in exceptedVersions:
-			addonWritePath = f"{self.testSet.outputDir}/{str(nvdaAPIVersion)}/{addon.addonId}"
+			addonWritePath = os.path.join(DATA_DIR.EXPECTED_RES, str(nvdaAPIVersion), addon.addonId)
 			Path(addonWritePath).mkdir(parents=True, exist_ok=True)
 			with open(f"{addonWritePath}/{addon.channel}.json", "w") as addonFile:
 				json.dump(addonData, addonFile, indent=4)
 
-	def write_nvdaAPIVersions(self):
-		Path(self.testSet.inputDir).mkdir(parents=True, exist_ok=True)
-		with open(f"{self.testSet.inputDir}/nvdaAPIVersions.json", "w") as nvdaAPIVersionFile:
-			nvdaAPIVersionsJson = [{
-				"description": str(version.apiVer),
-				"apiVer": version.apiVer._asdict(),
-				"backCompatTo": version.backCompatTo._asdict(),
-			} for version in self._test_nvdaAPIVersions]
+	@staticmethod
+	def write_nvdaAPIVersions():
+		"""Write mock NVDA API Versions data to the input."""
+		Path(DATA_DIR.INPUT.value).mkdir(parents=True, exist_ok=True)
+		nvdaAPIVersionsJson = [{
+			"description": str(version.apiVer),
+			"apiVer": version.apiVer._asdict(),
+			"backCompatTo": version.backCompatTo._asdict(),
+		} for version in _TestDataGenerator._test_nvdaAPIVersions]
+		with open(DATA_DIR.nvdaAPIVersionsPath.value, "w") as nvdaAPIVersionFile:
 			json.dump(nvdaAPIVersionsJson, nvdaAPIVersionFile)
 
-	def test_addon_to_be_fully_removed(self):
-		"""Creates addon data for an addon to be fully removed in subsequent datasets"""
-		if self.testSet.id != 0:
-			return
+	@staticmethod
+	def generate_addon():
+		"""Creates addon data for a lone addon which works of all NVDA API versions"""
+		availableApiVersions = [v.apiVer for v in _TestDataGenerator._test_nvdaAPIVersions]
 		addon = MockAddon()
-		addon.addonId = "fullyRemoved"
+		addon.addonId = "exampleAddon"
 		addon.addonVersion = MajorMinorPatch(0, 1)
-		addon.minNvdaAPIVersion = MajorMinorPatch(2020, 1)
-		addon.lastTestedVersion = MajorMinorPatch(2021, 1)
+		addon.minNvdaAPIVersion = min(availableApiVersions)
+		addon.lastTestedVersion = max(availableApiVersions)
 		addon.channel = "stable"
-		self.write_mock_addon_to_files(addon, [v.apiVer for v in self._test_nvdaAPIVersions])
+		_TestDataGenerator.write_mock_addon_to_files(addon, availableApiVersions)
 
-	def test_addon_to_be_downgraded(self):
-		"""Creates addon data for an addon to be downgraded in subsequent datasets"""
-		# stable version
+	@staticmethod
+	def generate_addon_with_versions():
+		"""Creates addon data to test the paths are correct across NVDA API Versions"""
+		# oldest version, to remain unlisted
 		addon = MockAddon()
-		addon.addonId = "downgraded"
-		addon.addonVersion = MajorMinorPatch(0, 9)
+		addon.addonId = "multipleVersionsAddon"
+		addon.channel = "stable"
+		addon.addonVersion = MajorMinorPatch(2, 1)
 		addon.minNvdaAPIVersion = MajorMinorPatch(2020, 1)
 		addon.lastTestedVersion = MajorMinorPatch(2020, 4)
-		addon.channel = "stable"
-		exceptedStableVersions = [MajorMinorPatch(2020, 1), MajorMinorPatch(2020, 2)]
-		if self.testSet.id == 1:
-			exceptedStableVersions.extend((MajorMinorPatch(2020, 3), MajorMinorPatch(2020, 4)))
-		self.write_mock_addon_to_files(addon, exceptedStableVersions)
+		_TestDataGenerator.write_mock_addon_to_files(addon, {MajorMinorPatch(2020, 1), MajorMinorPatch(2020, 2)})
 
-		if self.testSet.id == 0:
-			# version to be downgraded, removed in the second set
-			addon = deepcopy(addon)
-			addon.addonVersion = MajorMinorPatch(1, 1, 1)
-			addon.minNvdaAPIVersion = MajorMinorPatch(2020, 3)
-			addon.lastTestedVersion = MajorMinorPatch(2021, 1)
-			self.write_mock_addon_to_files(
-				addon,
-				{MajorMinorPatch(2020, 3), MajorMinorPatch(2020, 4), MajorMinorPatch(2021, 1)}
-			)
-
-	def test_stable_from_beta(self):
-		"""Generates a beta addon for the first set, and the equivalent stable addon for the second
-		"""
-		addon = MockAddon()
-		addon.addonId = "betaToStable"
-		addon.addonVersion = MajorMinorPatch(1, 1)
-		addon.minNvdaAPIVersion = MajorMinorPatch(2020, 1)
-		addon.lastTestedVersion = MajorMinorPatch(2020, 2)
-		addon.channel = "beta" if self.testSet.id == 0 else "stable"
-		self.write_mock_addon_to_files(
-			addon,
-			{MajorMinorPatch(2020, 1), MajorMinorPatch(2020, 2), MajorMinorPatch(2020, 3)}
-		)
-
-	def test_nvdaAPIVersions(self):
-		"""Creates addon data to test that an addon is written across the correct NVDA versions"""
-		addon = MockAddon()
-		addon.addonId = "_test_nvdaAPIVersions"
-		addon.addonVersion = MajorMinorPatch(1, 0)
-		addon.minNvdaAPIVersion = MajorMinorPatch(2020, 1)
-		addon.lastTestedVersion = MajorMinorPatch(2020, 2)
-		addon.channel = "stable"
-		self.write_mock_addon_to_files(addon, {
-			MajorMinorPatch(2020, 1),
-			MajorMinorPatch(2020, 2),
-			MajorMinorPatch(2020, 3),
-		})
+		# newer version
+		addon = deepcopy(addon)
+		addon.addonVersion = MajorMinorPatch(13, 0)
+		addon.minNvdaAPIVersion = MajorMinorPatch(2020, 3)
+		addon.lastTestedVersion = MajorMinorPatch(2021, 1)
+		_TestDataGenerator.write_mock_addon_to_files(addon, {MajorMinorPatch(2021, 1)})
 
 
 class TestTransformation(unittest.TestCase):
-	testSets: Tuple[TestSet] = (
-		TestSet(
-			id=0,
-			inputDir=os.path.join(os.path.dirname(__file__), "test_data/input"),
-			outputDir=os.path.join(os.path.dirname(__file__), "test_data/expected_results"),
-		),
-		TestSet(
-			id=1,
-			inputDir=os.path.join(os.path.dirname(__file__), "test_data/input"),
-			outputDir=os.path.join(os.path.dirname(__file__), "test_data/expected_results"),
-		),
-	)
-
 	@classmethod
-	def _execute_transformation(cls, testSet: TestSet):
-		if Path(TEST_OUTPUT_DIR).exists():
-			shutil.rmtree(TEST_OUTPUT_DIR)
-		nvdaAPIVersionsPath = f"{testSet.inputDir}/nvdaAPIVersions.json"
-		process = subprocess.run(
-			f"python -m src.transform {nvdaAPIVersionsPath} {testSet.inputDir} {TEST_OUTPUT_DIR}",
-			shell=True
-		)
-		process.check_returncode()
+	def setUpClass(cls):
+		"""Empty the test data before the start of tests"""
+		if Path(DATA_DIR._root.value).exists():
+			shutil.rmtree(DATA_DIR._root.value)
 
-	def _check_expected_addons_added(self, expectedResultsPath: str):
+	def tearDown(self):
+		"""Empty the test data after each test"""
+		if Path(DATA_DIR._root.value).exists():
+			shutil.rmtree(DATA_DIR._root.value)
+
+	def _check_expected_addons_added(self):
 		"""
 		Checks that all the data and files in expectedResultsPath match the test output directory.
 		"""
-		testOutputFiles = set(glob.glob(TEST_OUTPUT_DIR + VIEWS_GLOB))
-		expectedFiles = set(glob.glob(expectedResultsPath + VIEWS_GLOB))
-		testOutputFiles = set(f.lstrip(TEST_OUTPUT_DIR) for f in testOutputFiles)
-		expectedFiles = set(f.lstrip(expectedResultsPath) for f in expectedFiles)
+		testOutputFiles = set(glob.glob(DATA_DIR.OUTPUT + VIEWS_GLOB))
+		expectedFiles = set(glob.glob(DATA_DIR.EXPECTED_RES + VIEWS_GLOB))
+		testOutputFiles = set(f.lstrip(DATA_DIR.OUTPUT) for f in testOutputFiles)
+		expectedFiles = set(f.lstrip(DATA_DIR.EXPECTED_RES) for f in expectedFiles)
 		self.assertEqual(testOutputFiles, expectedFiles)
 		for outputFilename in testOutputFiles:
-			with open(f"{TEST_OUTPUT_DIR}/{outputFilename}", "r") as outputFile:
+			with open(f"{DATA_DIR.OUTPUT}/{outputFilename}", "r") as outputFile:
 				outputFileJson = json.load(outputFile)
-			with open(f"{expectedResultsPath}/{outputFilename}", "r") as expectedFile:
+			with open(f"{DATA_DIR.EXPECTED_RES}/{outputFilename}", "r") as expectedFile:
 				expectedResultsJson = json.load(expectedFile)
 			self.assertDictEqual(expectedResultsJson, outputFileJson, msg=outputFilename)
 
-	def test_addon_to_be_fully_removed(self):
+	def _test_transform(self) -> None:
+		"""Generates NVDA API versions so that transform can run.
 		"""
-		Confirms that a subsequent transformation is successful, and the first transformation results are
-		overwritten with a fully removed addon.
-		"""
-		_GenerateSystemTestData(self.testSets[0]).test_addon_to_be_fully_removed()
-		self._execute_transformation(self.testSets[0])
-		self._check_expected_addons_added(self.testSets[0].outputDir)
-		_GenerateSystemTestData(self.testSets[1]).test_addon_to_be_fully_removed()
-		self._execute_transformation(self.testSets[1])
-		self._check_expected_addons_added(self.testSets[1].outputDir)
+		_TestDataGenerator.write_nvdaAPIVersions()
+		subprocess.run(
+			f"python -m src.transform {DATA_DIR.nvdaAPIVersionsPath} {DATA_DIR.INPUT} {DATA_DIR.OUTPUT}",
+			shell=True
+		).check_returncode()  # Raise CalledProcessError if the exit code is non-zero.
 
-	def test_addon_to_be_downgraded(self):
+	def test_transform_empty(self):
+		"""Confirms an empty transformation is successful
 		"""
-		Confirms that a subsequent transformation is successful, and the first transformation results are
-		overwritten with a downgraded addon.
-		"""
-		_GenerateSystemTestData(self.testSets[0]).test_addon_to_be_downgraded()
-		self._execute_transformation(self.testSets[0])
-		self._check_expected_addons_added(self.testSets[0].outputDir)
-		_GenerateSystemTestData(self.testSets[1]).test_addon_to_be_downgraded()
-		self._execute_transformation(self.testSets[1])
-		self._check_expected_addons_added(self.testSets[1].outputDir)
+		self._test_transform()
 
-	def test_nvdaAPIVersions(self):
+	def test_transform_successfully(self):
+		"""Confirms an transformation of a single addon is successful
 		"""
-		Confirms that the newest addon versions are used and added to the correct NVDA API folder.
-		"""
-		_GenerateSystemTestData(self.testSets[0]).test_nvdaAPIVersions()
-		self._execute_transformation(self.testSets[0])
-		self._check_expected_addons_added(self.testSets[0].outputDir)
-		_GenerateSystemTestData(self.testSets[1]).test_nvdaAPIVersions()
-		self._execute_transformation(self.testSets[1])
-		self._check_expected_addons_added(self.testSets[1].outputDir)
+		_TestDataGenerator.generate_addon()
+		self._test_transform()
 
-	def test_stable_from_beta(self):
+	def test_throw_error_on_nonempty_output_folder(self):
+		"""Confirms using an existing output directory throws an error
 		"""
-		Confirms that a subsequent transformation is successful, and the first transformation results are
-		overwritten with an addon that has gone to stable from beta.
+		# Make the folder before transform
+		Path(DATA_DIR.OUTPUT.value).mkdir(parents=True, exist_ok=True)
+		
+		with self.assertRaises(subprocess.CalledProcessError):
+			self._test_transform()
+
+	def test_output_file_structure_matches_expected(self):
+		"""Confirms that a successful transform of multiple addons
+		is written as expected.
 		"""
-		_GenerateSystemTestData(self.testSets[0]).test_stable_from_beta()
-		self._execute_transformation(self.testSets[0])
-		self._check_expected_addons_added(self.testSets[0].outputDir)
-		_GenerateSystemTestData(self.testSets[1]).test_stable_from_beta()
-		self._execute_transformation(self.testSets[1])
-		self._check_expected_addons_added(self.testSets[1].outputDir)
+		_TestDataGenerator.generate_addon()
+		_TestDataGenerator.generate_addon_with_versions()
+		self._test_transform()
+		self._check_expected_addons_added()
